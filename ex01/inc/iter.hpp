002b38602487d0080b15e8903731256b67d52aa2
DejVV <33099962+dawidtanski@users.noreply.github.com>
2025-12-02 18:48:10 +0100

    Add files via upload

--- ex01/inc/iter.hpp
+++ ex01/inc/iter.hpp
@@ -3,8 +3,8 @@
 
 // Template argumnet deduction. Compilator looks at type of argument and fill in T
 template <typename T, typename F>
-void iter(T *array, const size_t length, F func){
-	for(size_t i = 0; i < arrLen, i++){
-		F(array[i]);
+void iter(T *array, const size_t arrLen, F func){
+	for(size_t i = 0; i < arrLen; i++){
+		func(array[i]);
 	}
 }
--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,10 +1,19 @@
 #include "../inc/iter.hpp"
 #include <iostream>
 
+int iincrease(int &i){
+	i++;
+	return i;
+}
+
 int main(void){
 	int num1 = 1;
-	int num2 = 1;
+	int num2 = 2;
 	int num3 = 1;
-	int *array = {*num1, *num2, *num3};
-	iter()
+	int array[] = {num1, num2, num3};
+	for(int i = 0; i < 3; i++)
+		std::cout << array[i] << std::endl;
+	iter(array, 3, iincrease);
+	for(int i = 0; i < 3; i++)
+		std::cout << array[i] << std::endl;
 }
