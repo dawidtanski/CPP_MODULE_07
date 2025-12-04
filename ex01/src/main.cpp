e3859f4ce44b323f1594333aa7ddb4f4be928a20
DejVV <33099962+dawidtanski@users.noreply.github.com>
2025-12-04 18:17:51 +0100

    Add files via upload

--- ex01/src/main.cpp
+++ ex01/src/main.cpp
@@ -1,19 +1,21 @@
 #include "../inc/iter.hpp"
 #include <iostream>
 
-int iincrease(int &i){
-	i++;
-	return i;
-}
+	int increase(int i){
+		return ++i;
+	}
 
 int main(void){
 	int num1 = 1;
 	int num2 = 2;
-	int num3 = 1;
+	int num3 = 3;
+
+	
 	int array[] = {num1, num2, num3};
-	for(int i = 0; i < 3; i++)
+	for(int i = 0; i < 3; i++){
 		std::cout << array[i] << std::endl;
-	iter(array, 3, iincrease);
-	for(int i = 0; i < 3; i++)
+		iter(array, 3, increase );
 		std::cout << array[i] << std::endl;
+	}
+
 }
